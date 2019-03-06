9207d75096cfa848e5aee0f8e713a4f868c8efb0
Daan Soeten <d.soeten@gmail.com>
2019-03-06 17:21:24 +0100

    Squashed some bugs & added example output to readme

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ import json
 page = 1
 data = []
 
-
 while True:
     r = requests.get('https://api.magicthegathering.io/v1/cards?page=' + str(page))
 
     print('Requesting cards from page ' + str(page))
 
-    if r.status_code != 200:
-        with open('data.json', 'w') as outfile:
-            json.dump(data, 'cards.json')
+    if r.status_code != 200 or not json.loads(r.content.decode())['cards']:
+        with open('cards.json', 'w') as outfile:
+            json.dump(data, outfile)
 
-        print('Finished after looping through ')
+        print('Finished after looping through ' + str(page) + ' pages with ' + str(len(data)) + ' results')
         exit(0)
     else:
         page += 1
-
         data.extend(json.loads(r.content.decode())['cards'])
 
 
